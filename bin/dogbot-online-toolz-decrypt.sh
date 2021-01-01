de42036e0d6fb5091c59b81dde68e3bde4b430e9
fffuture.work <71678343+fffuturework@users.noreply.github.com>
2021-01-01 20:19:49 +0900

    Create dogbot-online-toolz-decrypt.sh

---
+++ bin/dogbot-online-toolz-decrypt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+HEADLESS=true DB_SOURCE='https://gist.githubusercontent.com/fffuturework/0b6454fe0303c5003ff083cdab7334bc/raw/211de30698a255e7eb6bef08eb22a847f85661cb/dogbot-online-toolz-decrypt.js' OT_ENCRYPTED=$1 npx codecept
+js run --steps
